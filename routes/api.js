70add70362af646ba63f04f3eec1b2a204b8ae11
Simon Cooper <kidney9@hotmail.com>
2018-09-10 12:00:08 +0100

    Routes added

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,15 @@ var expect = require('chai').expect;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .get((req, res) => {
+      // send html file
+      res.sendfile(process.cwd() + '/views/thread.html')
+    })
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+    .get((req, res) => {
+      res.sendfile(process.cwd() + '/views/board.html')
+    })
 
 };
