13e96e192af7d380c883ff3f7eee1da6e77e9109
Simon Cooper <kidney9@hotmail.com>
2018-09-18 14:42:19 +0100

    helmetjs functions added, apiRoutes + fccTestingRoutes order reversed

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 const mongoose = require('mongoose');
-// require('./models/Reply');
-// require('./models/Thread');
-// require('./models/Board');
+const helmet = require('helmet');
 
 require('dotenv').config();
 
@@ -26,6 +24,15 @@ if (process.env.NODE_ENV === 'test') {
   mongoose.connect(process.env.MONGO_URL_DEV, { useNewUrlParser: true });
 }
 
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
@@ -33,31 +40,12 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-// app.route('/b/:board/')
-//   .get(function (req, res) {
-//     res.sendFile(process.cwd() + '/views/board.html');
-//   });
-// app.route('/b/:board/:threadid')
-//   .get(function (req, res) {
-//     res.sendFile(process.cwd() + '/views/thread.html');
-//   });
-
-//Index page (static HTML)
-// app.route('/')
-//   .get(function (req, res) {
-//     res.sendFile(process.cwd() + '/views/index.html');
-//   });
+//Routing for API 
+apiRoutes(app);
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);
-
-//Sample Front-end
-
-    
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
